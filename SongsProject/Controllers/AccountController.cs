0f8434a45216ad631a59374ba69ce10953a2ec1a
DimaSpektor <dimaspektor1@gmail.com>
2019-11-04 20:19:59 +0200

    UpdateChangePassword

--- SongsProject/Controllers/AccountController.cs
+++ SongsProject/Controllers/AccountController.cs
@@ -419,8 +419,17 @@ namespace SongsProject.Controllers
         }
 
         [HttpGet]
-        public IActionResult ChangePassword()
+        public async Task<IActionResult> ChangePassword()
         {
+            var user = await _userManager.GetUserAsync(User);
+
+            var userHasPassword = await _userManager.HasPasswordAsync(user);
+
+            if (!userHasPassword)
+            {
+                return RedirectToAction("AddPassword");
+            }
+
             return View();
         }
 
@@ -454,6 +463,47 @@ namespace SongsProject.Controllers
                 // Upon successfully changing the password refresh sign-in cookie
                 await _signInManager.RefreshSignInAsync(user);
                 return View("ChangePasswordConfirmation");
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddPassword()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            var userHasPassword = await _userManager.HasPasswordAsync(user);
+
+            if (userHasPassword)
+            {
+                return RedirectToAction("ChangePassword");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPassword(AddPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+
+                var result = await _userManager.AddPasswordAsync(user, model.NewPassword);
+
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View();
+                }
+                // Refresh user sign-in cookie
+                await _signInManager.RefreshSignInAsync(user);
+
+                return View("AddPasswordConfirmation");
             }
 
             return View(model);
