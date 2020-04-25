33710c3afecdbdc43282484c969fb27643da709f
DimaSpektor <dorsaaran12@gmail.com>
2020-04-26 02:38:52 +0300

    Update

--- SongsProject/wwwroot/js/CustomScript.js
+++ SongsProject/wwwroot/js/CustomScript.js
@@ -1,4 +1,5 @@
-﻿function confirmDelete(uniqueId, isDeleteClicked) {
+﻿/*-- Function that check if the user want to delete the user or role */
+function confirmDelete(uniqueId, isDeleteClicked) {
     var deleteSpan = 'deleteSpan_' + uniqueId;
     var confirmDeleteSpan = 'confirmDeleteSpan_' + uniqueId;
 
